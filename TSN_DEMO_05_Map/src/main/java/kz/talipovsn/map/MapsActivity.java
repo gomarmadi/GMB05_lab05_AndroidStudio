7623859fc2d5eb7647aacd74112ed297c70deb8a
Madi <71094011+gomarmadi@users.noreply.github.com>
2022-11-15 09:38:01 +0600

    Add files via upload

---
+++ TSN_DEMO_05_Map/src/main/java/kz/talipovsn/map/MapsActivity.java
@@ -0,0 +1,210 @@
+package kz.talipovsn.map;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap; // Карта
+    private TextView textView; // Текстовый компонет
+
+    private String markerTitle = ""; // Название выбранного маркера
+    private String markerFileName = ""; // Имя файла с подробными данными выбранного маркера
+
+    private final int SITYSCALE = 17; // Масштаб для отображения карты
+
+    static final LatLng startMarker = new LatLng(52.272613,76.971165); // Начальный маркер
+    static final LatLng marker1 = new LatLng(52.273312,76.967841); // Маркер Белый Ветер
+    static final LatLng marker2 = new LatLng(52.274035,76.966727); // Маркер Wildberries
+    static final LatLng marker3 = new LatLng(52.2693893,76.9704737); // Маркер БИЛ
+    static final LatLng marker4 = new LatLng(52.2756535,76.9697155); // Маркер Евразийского Банка
+    static final LatLng marker5 = new LatLng(52.273655,76.972733); // Маркер Учебный Куис
+    private static final float ALPHA = 0.8f; // Коэффициент прозрачности для маркеров
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Доступ к карте
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        textView = findViewById(R.id.textViewInfo); // Доступ к компоненту "textViewInfo"
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Обычный тип карты
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Добавление маркера на карту с текстом
+        mMap.addMarker(new MarkerOptions().position(startMarker).title((getString(R.string.startMarker_title))));
+
+        // Добавление маркера на карту с текстом, иконкой и полупрозрачностью
+        mMap.addMarker(new MarkerOptions().position(marker1).title(getString(R.string.marker1_title)).icon(
+                BitmapDescriptorFactory.fromResource(R.drawable.bv)).alpha(ALPHA).
+                snippet(getString(R.string.marker1_txt_click)));
+
+        // Добавление маркера на карту с текстом, иконкой и полупрозрачностью
+        mMap.addMarker(new MarkerOptions().position(marker2).title((getString(R.string.marker2_title))).icon(
+                BitmapDescriptorFactory.fromResource(R.drawable.wb)).alpha(ALPHA).
+                snippet(getString(R.string.marker2_txt_click)));
+
+        // Добавление маркера на карту с текстом, иконкой и полупрозрачностью
+        mMap.addMarker(new MarkerOptions().position(marker3).title((getString(R.string.marker3_title))).icon(
+                        BitmapDescriptorFactory.fromResource(R.drawable.bil)).alpha(ALPHA).
+                snippet(getString(R.string.marker3_txt_click)));
+
+        // Добавление маркера на карту с текстом, иконкой и полупрозрачностью
+        mMap.addMarker(new MarkerOptions().position(marker4).title((getString(R.string.marker4_title))).icon(
+                        BitmapDescriptorFactory.fromResource(R.drawable.eur)).alpha(ALPHA).
+                snippet(getString(R.string.marker4_txt_click)));
+
+        // Добавление маркера на карту с текстом, иконкой и полупрозрачностью
+        mMap.addMarker(new MarkerOptions().position(marker5).title((getString(R.string.marker5_title))).icon(
+                        BitmapDescriptorFactory.fromResource(R.drawable.kuis)).alpha(ALPHA).
+                snippet(getString(R.string.marker5_txt_click)));
+
+        //Разрешение изменения масштаба карты
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Проверка на включенный GPS и позиционирование на карте
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //Показать текущее местоположение по GPS
+            mMap.setMyLocationEnabled(true);
+        }
+
+        // Переход просмотра на карте на нужный маркер c зумом
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startMarker, SITYSCALE));
+
+        // Инициализация стартового маркера
+        onMarkerClick(getString(R.string.startMarker_id));
+
+        // Обработчик нажатия на маркеры карты
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.showInfoWindow();
+                MapsActivity.this.onMarkerClick(marker.getId());
+                return true;
+            }
+        });
+    }
+
+    // Нажатие на маркер
+    public void onMarkerClick(String idMarker) {
+        switch (idMarker) {
+            case "m0":
+                doClickMarker(startMarker, getString(R.string.startMarker_info),
+                        getString(R.string.startMarker_title), getString(R.string.startMarker_file));
+                break;
+            case "m1":
+                doClickMarker(marker1, getString(R.string.marker1_info),
+                        getString(R.string.marker1_title), getString(R.string.marker1_file));
+                break;
+            case "m2":
+                doClickMarker(marker2, getString(R.string.marker2_info),
+                        getString(R.string.marker2_title), getString(R.string.marker2_file));
+                break;
+            case "m3":
+                doClickMarker(marker3, getString(R.string.marker3_info),
+                        getString(R.string.marker3_title), getString(R.string.marker3_file));
+                break;
+            case "m4":
+                doClickMarker(marker4, getString(R.string.marker4_info),
+                        getString(R.string.marker4_title), getString(R.string.marker4_file));
+                break;
+            case "m5":
+                doClickMarker(marker5, getString(R.string.marker5_info),
+                        getString(R.string.marker5_title), getString(R.string.marker5_file));
+                break;
+            case "start":
+                doClickMarker(startMarker, getString(R.string.startMarker_info),
+                        getString(R.string.startMarker_title), getString(R.string.startMarker_file));
+                break;
+
+        }
+    }
+
+    // Обработка нажатия на маркер
+    public void doClickMarker(LatLng marker, String info, String markerTitle, String markerFileName) {
+        this.markerTitle = markerTitle;
+        this.markerFileName = markerFileName;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, SITYSCALE));
+        findViewById(R.id.sv1).scrollTo(0, 0);
+        if (Build.VERSION.SDK_INT >= 24) {
+            textView.setText(Html.fromHtml(info, Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            textView.setText(Html.fromHtml(info));
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    //Меню
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.normal:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.satellite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+        }
+        //headerView.setText(item.getTitle());
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Нажатие на кнопку маркера
+    public void onClickButtonMarker(View view) {
+        String idMarker = view.getTag().toString();
+        onMarkerClick(idMarker);
+    }
+
+    // Обработчик кнопки "Подробно"
+    public void detailButtonClick(View view) {
+        if (!markerFileName.equals("")) {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(getString(R.string.tMarker), markerTitle);
+            intent.putExtra(getString(R.string.mfile), markerFileName);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.selectOb, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
